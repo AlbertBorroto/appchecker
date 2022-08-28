f2dbc2022efecd13f7e0f69737df247b0b8c9b31
albert <albert.borroto@palmbeachschools.org>
2022-08-28 15:32:00 -0400

    WIP need HTML name values

--- auto_login_appchecker.py
+++ auto_login_appchecker.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # urls for critical appchecks
-from selenium.webdriver.common.by import By
-
 urls = ['https://docs.google.com/spreadsheets/d/1mEhLZnwmX0VacWxECbEFmNLuXVizO05C1lu9e5mZ-Vs/edit#gid=637365405',
         'https://mysdpbc.org/',
         'https://www.palmbeachschools.org/',
@@ -28,9 +29,47 @@ urls = ['https://docs.google.com/spreadsheets/d/1mEhLZnwmX0VacWxECbEFmNLuXVizO05
         'https://10.254.18.202/ui/#/login'
         ]
 
+# take user's password and username
+usernamestr = input('Please input your username: ')
+passwordstr = input('Please input your password, use correct capitalization: ')  # todo obfuscate entry field
+
+# webdriver to control chrome
 browser = webdriver.Chrome()
 
+
+# takes in the html field names and logs in
+def login(username_field, password_field, submit_field):
+    username = browser.find_element(By.NAME, username_field)
+    username.send_keys(usernamestr)
+
+    password = browser.find_element(By.NAME, password_field)
+    password.send_keys(passwordstr)
+
+    submit_button = browser.find_element(By.NAME, submit_field)
+    submit_button.click()
+
+    time.sleep(2)
+
+
+# open urls and login if necessary
 for url in urls:
     browser.get(url)
-    browser.find_element(By.NAME, '')
+    time.sleep(15)
 
+    if 'erp' in url:
+        login('userid', 'pwd', 'Submit')
+
+    elif 'edw' in url:
+        login()
+        # todo find html name values
+
+    elif 'files' in url:
+        login()
+        # todo find html name values
+
+    elif 'transintranet' in url:
+        login()
+        # todo find html name values
+
+    else:
+        next()
